2465b32469286a0d8749aef02df9edb94cbde2a2
Gabriella Fadel <gabriellafadelbraz@outlook.com>
2023-08-05 11:42:24 -0300

    create a new file Calculator - Isolate the operations - use import method

---
+++ BasicOperations/Calculator.py
@@ -0,0 +1,11 @@
+import Operations
+
+print ("Hello, Wellcome to the Basic Calculator")
+
+value_one = int(input("Enter with the first value:"))
+value_two = int(input("Enter with the second value:"))
+
+# writer note : if you want concatenete a int variable in python you need convert it for type string, like the exemple 
+print("the fisrt value is: "+(str(value_one))+" the second value is: "+(str(value_two))) 
+
+Operations.Operations.division(value_one,value_two)
--- BasicOperations/Operations.py
+++ BasicOperations/Operations.py
@@ -1,14 +1,3 @@
-print ("Hello, this is a class with simple mathematics operations")
-
-value_one = int(input("Enter with the first value:"))
-value_two = int(input("Enter with the second value:"))
-
-if (value_one <= 0):
-    print("the first value must to be bigger than 0")
-    value_one = int(input("Please enter with another value:"))
-
-# writer note : if you want concatenete a int variable in python you need convert it for type string, like the exemple 
-print("the fisrt value is: "+(str(value_one))+" the second value is: "+(str(value_two))) 
 
 class Operations:
 
@@ -30,11 +19,11 @@ class Operations:
 
     #Division 
     def division(val_1,val_2):
-        division  = val_1 / val_2
+        #this  division aways return a result of type integer
+        division  = val_1 // val_2
         print("the result of division is:"+(str(division)))
 
 
-Operations.addition(value_one,value_two)
 
 
 
